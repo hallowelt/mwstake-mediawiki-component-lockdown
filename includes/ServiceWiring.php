16c1395a4a585f8c7a10b1f236cb31bb9f1acc06
pwirth <wirth@hallowelt.biz>
2021-11-11 15:10:11 +0100

    Migrate from BSFoundation pt. I

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -4,6 +4,8 @@ use MediaWiki\MediaWikiServices;
 
 return [
 	'MWStakeLockdownFactory' => function ( MediaWikiServices $services ) {
-		return new LockdownFactory( $GLOBALS['mwsgLockdownRegistry'] );
+		$registry = $services->getService( 'MWStakeManifestRegistryFactory' )
+			->get( 'MWStakeLockdownRegistry' );
+		return new LockdownFactory( $registry, $GLOBALS['mwsgLockdownRegistry'] );
 	}
 ];
